e5f53a25bdcd6981c19c6bf10be9373537c239d3
Your Name <you@example.com>
2025-10-22 13:01:09 +0300

    club members uzdevums

---
+++ klubs.js
@@ -0,0 +1,10 @@
+let memberList = [
+    {Vārds : "Kārlis", Uzvārds : "Stūrmanis", loma : "leader", Stundas :24},
+    {Vārds : "Tifānija", Uzvārds : "Smekša", loma : "leader", Stundas :18},
+    {Vārds : "Melānija", Uzvārds : "Mieze", loma : "member", Stundas :15},
+    {Vārds : "Kristers", Uzvārds : "Kukulis", loma : "member", Stundas :21},
+    {Vārds : "Niks", Uzvārds : "Zoltners", loma : "member", Stundas :7},
+    {Vārds : "Armands", Uzvārds : "Ābele", loma : "member", Stundas :11},
+    {Vārds : "Lelde", Uzvārds : "Logina", loma : "member", Stundas :16},
+]
+
